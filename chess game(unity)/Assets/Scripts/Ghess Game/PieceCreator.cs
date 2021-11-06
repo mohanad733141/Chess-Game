3967694e358252289cbe7655c8092778e363ac1e
jaswag <93076086+jaswag@users.noreply.github.com>
2021-11-06 20:57:50 +0000

    Update PieceCreator.cs

--- chess game(unity)/Assets/Scripts/Ghess Game/PieceCreator.cs
+++ chess game(unity)/Assets/Scripts/Ghess Game/PieceCreator.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class PieceCreator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private GameObject[] piecesPrefabs;
+    [SerializeField] private Material blcMat;
+    [SerializeField] private Material whiteMat;
+
+    private Dictionary<string, GameObject> nameToPieceDict = new Dictionary<string, GameObject>;
+
+    private void Awake()
     {
-        
+        foreach (var piece in piecesPrefabs)
+        {
+            nameToPieceDict.Add(piece.GetComponent<Piece>().GetType().ToString(), piece);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public GameObject CreatePiece(Type type)
     {
-        
+        GameObject prefab = nameToPieceDict[type.ToString()];
+        if(prefab)
+        {
+            GameObject newPiece = Instantiate(prefab);
+        }
+        return null;
+    }
+
+    public Material getTeamMaterial(TeamColor team)
+    {
+        if(team == whiteMat)
+        {
+            return whiteMat;
+        } else
+        {
+            return blcMat;
+        }
     }
 }
