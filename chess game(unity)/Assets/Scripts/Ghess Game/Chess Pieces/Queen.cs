500968f4953483746c174660764163d063bbb485
mohanad733141 <78827810+mohanad733141@users.noreply.github.com>
2021-11-21 01:37:21 +0000

    update

--- chess game(unity)/Assets/Scripts/Ghess Game/Chess Pieces/Queen.cs
+++ chess game(unity)/Assets/Scripts/Ghess Game/Chess Pieces/Queen.cs
@@ -6,11 +6,7 @@ public class Queen : Piece
 {
     public override List<Vector2Int> SelectAvailableCells()
     {
-<<<<<<< HEAD
         movesAvailable.Clear();
-=======
-         movesAvailable.Clear();
->>>>>>> parent of f9e4564 (Revert "trying to fix some errors")
         movesAvailable.Add(unavailableCell + new Vector2Int(0, 1));
         return movesAvailable;
     }
