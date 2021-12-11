48797a536ac2c59c35585408a1866e87089a0d4f
Yashla <yashdhayani900@gmail.com>
2021-12-11 01:53:47 +0000

    yash network again

--- chess game(unity)/Assets/Scripts/Ghess Game/Input System/BoardInputHandler.cs
+++ chess game(unity)/Assets/Scripts/Ghess Game/Input System/BoardInputHandler.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(ChessBoard))]
 public class BoardInputHandler : MonoBehaviour, IInputHandler
 {
     private ChessBoard board;
